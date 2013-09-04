0db82b6d89ef27721d92028c2c0b4c8a274d3d31
Boris Marin <borismarin@gmail.com>
2013-09-04 19:54:26 +0100

    added xpp visitor

--- visitors.py
+++ visitors.py
@@ -373,3 +373,130 @@ class Latex(ast.NodeVisitor):
 
     def generic_prec(self, n):
         return 0
+
+
+
+class xpp(ast.NodeVisitor):
+
+    def prec(self, n):
+        return getattr(self, 'prec_'+n.__class__.__name__, getattr(self, 'generic_prec'))(n)
+
+    def visit_Call(self, n):
+        func = self.visit(n.func)
+        args = ', '.join(map(self.visit, n.args))
+        return r'%s(%s)' % (func, args)
+
+    def prec_Call(self, n):
+        return 1000
+
+    def visit_Name(self, n):
+        return n.id
+
+    def prec_Name(self, n):
+        return 1000
+
+    def visit_UnaryOp(self, n):
+        if self.prec(n.op) > self.prec(n.operand):
+            return r'%s (%s)' % (self.visit(n.op), self.visit(n.operand))
+        else:
+            return r'%s %s' % (self.visit(n.op), self.visit(n.operand))
+
+    def prec_UnaryOp(self, n):
+        return self.prec(n.op)
+
+    def visit_BinOp(self, n):
+        if self.prec(n.op) > self.prec(n.left):
+            left = r'(%s)' % self.visit(n.left)
+        else:
+            left = self.visit(n.left)
+        if self.prec(n.op) > self.prec(n.right):
+            right = r'(%s)' % self.visit(n.right)
+        else:
+            right = self.visit(n.right)
+        if isinstance(n.op, ast.FloorDiv):
+            return r'flr(%s/%s)' % (self.visit(n.left), self.visit(n.right))
+        elif isinstance(n.op, ast.BitXor):
+            return r'%s ^ %s' % (self.visit(n.left), self.visit(n.right))
+        elif isinstance(n.op, ast.Pow):
+            return r'%s ^ %s' % (left, self.visit(n.right))
+        if isinstance(n.op, ast.Mod):
+            return r'mod(%s,%s)' % (self.visit(n.left), self.visit(n.right))
+        else:
+            return r'%s %s %s' % (left, self.visit(n.op), right)
+
+    def prec_BinOp(self, n):
+        return self.prec(n.op)
+
+    def visit_Sub(self, n):
+        return '-'
+
+    def prec_Sub(self, n):
+        return 300
+
+    def visit_Add(self, n):
+        return '+'
+
+    def prec_Add(self, n):
+        return 300
+
+    def visit_Mult(self, n):
+        return '*'
+
+    def prec_Mult(self, n):
+        return 400
+
+    def visit_Mod(self, n):
+        return 'mod'
+
+    def prec_Mod(self, n):
+        return 500
+
+    def prec_Pow(self, n):
+        return 700
+
+    def visit_Div(self, n):
+        return '/'
+
+    def prec_Div(self, n):
+        return 400
+
+    def prec_FloorDiv(self, n):
+        return 400
+
+    def visit_Invert(self, n):
+        return '-'
+
+    def prec_Invert(self, n):
+        return 800
+
+    def visit_Not(self, n):
+        return '~'
+ 
+    def prec_Not(self, n):
+        return 800
+
+    def visit_UAdd(self, n):
+        return '+'
+
+    def prec_UAdd(self, n):
+        return 800
+
+    def visit_USub(self, n):
+        return '-'
+
+    def prec_USub(self, n):
+        return 800
+    def visit_Num(self, n):
+        return str(n.n)
+
+    def prec_Num(self, n):
+        return 1000
+
+    def generic_visit(self, n):
+        if isinstance(n, ast.AST):
+            return r'' % (n.__class__.__name__, ', '.join(map(self.visit, [getattr(n, f) for f in n._fields])))
+        else:
+            return str(n)
+
+    def generic_prec(self, n):
+        return 0
